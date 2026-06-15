fec15f4a51962ddf1ceabbd5c111897c9851267b
Jonas <jonasg@umich.edu>
2026-06-15 01:02:49 -0400

    More index scaffolding

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,4 @@
-use std::{fmt, fs::{File, remove_file}, io::{BufReader, BufRead}, path::PathBuf};
+use std::{collections::HashSet, fmt, fs::{File, remove_file}, io::{BufRead, BufReader}, path::PathBuf};
 
 use scraper::{Html, Selector};
 
@@ -24,27 +24,28 @@ impl fmt::Display for IndexSummary {
 }
 
 struct Dictionary {
-
+    dictionary: HashSet<String, u64>, // TODO: varint?
+    posting_lists: Vec<PostingList>,
 }
 
 struct PostingList {
-
+    posts: Vec<Post>,
 }
 
 struct Post {
-
+    docid_or_delta: u64, // TODO: varint
+    positions: Vec<u64>, // TODO: varint
 }
 
 struct DocStore {
-
+    // docid -> meta
+    docstore: HashSet<u64, DocMeta>,
 }
 
-// let document = Html::parse_document(&html);
-// for tag in [ "title", "h1", "p" ] {
-//     let selector =  Selector::parse(tag).unwrap();
-//     let words = parse_words(&document, &selector);
-//     index_document(words)?;
-// }
+struct DocMeta {
+    url: String,
+}
+
 pub fn parse_words(document: &Html, selector: &Selector) -> Vec<String> {
     document.select(&selector)
         .map(|element| element.text()
@@ -63,10 +64,18 @@ pub async fn index(crawl_file: PathBuf, consume: bool) -> Result<IndexSummary, N
     let file = File::open(&crawl_file)?;
     let reader = BufReader::new(file);
 
+    let mut docid = 0;
     for line in reader.lines() {
         let line = line?;
         let page: CrawledPage = serde_json::from_str(&line)?;
+        let document = Html::parse_document(&page.html);
+        for tag in [ "title", "h1", "p" ] {
+            let selector =  Selector::parse(tag).unwrap();
+            let words = parse_words(&document, &selector);
+        }
         eprintln!("indexing {}", page.url);
+
+        docid += 1;
     }
 
     if consume {
